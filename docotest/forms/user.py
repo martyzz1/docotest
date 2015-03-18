a82f763979b606b492e7e09af39addf445491c4f
Martyzz1 <martin@sharehood.co.uk>
2015-03-18 01:46:21 +0000

    Add the extra fields for the user Profile

--- docotest/forms/user.py
+++ docotest/forms/user.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.forms import extras
+from datetime import date
 
 
 class SignupForm(forms.Form):
-    first_name = forms.CharField(max_length=30, label='First Name')
-    last_name = forms.CharField(max_length=30, label='Surname')
-    dob = forms.DateField(widget=extras.SelectDateWidget, label='Date of Birth')
+    first_name = forms.CharField(max_length=30, label='First Name',
+                            widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30, label='Surname',
+                            widget=forms.TextInput(attrs={'placeholder': 'Surname'}))
+    dob = forms.DateField(widget=extras.SelectDateWidget(years=range(date.today().year, 1900, -1)), label='Date of Birth')
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
